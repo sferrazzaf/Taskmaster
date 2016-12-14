f4619bb05af1905a05db53f2374748e68d8d4702
sferrazzaf <s3francis@gmail.com>
2016-12-13 22:37:47 -0500

    Flesh out Task model and add idle and tasklist models

--- to_do_list/models.py
+++ to_do_list/models.py
@@ -2,10 +2,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 class Task(models.Model):
     text = models.CharField(max_length=200)
+    priority = models.IntegerField('priority', blank=True, null=True)
+    duration = models.DurationField('estimated duration', blank=True, null=True)
     created = models.DateTimeField('date created')
     completed = models.DateTimeField('date completed', blank=True, null=True)
+
+class Tasklist(models.Model):
+    name = models.CharField(max_length=100)
 
 class Starttask(models.Model):
     task = models.ForeignKey(Task)
@@ -15,5 +21,8 @@ class Stoptask(models.Model):
     task = models.ForeignKey(Task)
     time = models.DateTimeField('time stopped')
 
+class Idlestart(models.Model):
+    time = models.DateTimeField('idling started')
 
-# Create your models here.
+class Idlestop(models.Model):
+    time = models.DateTimeField('idling stopped')
