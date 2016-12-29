098cd5a7cfdf4921cbe66a0bdc99192434d939d5
sferrazzaf <s3francis@gmail.com>
2016-12-28 22:24:11 -0500

    set up form and HTML for duration input for new tasks

--- to_do_list/forms.py
+++ to_do_list/forms.py
@@ -2,3 +2,4 @@ from django import forms
 
 class TaskForm(forms.Form):
     text = forms.CharField(label='Enter new task', max_length=200)
+    duration = forms.DurationField(label='Enter expected duration in microseconds')
